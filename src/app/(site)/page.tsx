d315cd77d9468c053c08a5f3eeaf8105f1ac7989
thornie04 <kimchanthon46@gmail.com>
2026-08-22 18:15:14 +0700

    Remove banner for the top

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,19 +1,7 @@
-import { Suspense } from "react";
-
 import HomePageClient from "@/components/home/HomePageClient";
-import HeroBannerSection from "@/components/banners/HeroBannerSection";
-import { HeroBannerSkeleton } from "@/components/banners/BannerSectionSkeleton";
 
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return (
-    <>
-      <Suspense fallback={<HeroBannerSkeleton />}>
-        <HeroBannerSection />
-      </Suspense>
-
-      <HomePageClient />
-    </>
-  );
+  return <HomePageClient />;
 }
